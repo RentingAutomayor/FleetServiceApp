077615c639ee37c00d4b25e5ad91136c663f7fec
William Armando Forero Bermeo <williamforerob@gmail.com>
2021-01-21 00:15:10 -0500

    avance calculo de impuestos por item

---
+++ src/app/Modules/items-and-routines/pipes/calculate-taxes.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { MaintenanceItem } from 'src/app/Models/MaintenanceItem';
+
+@Pipe({
+  name: 'calculateTaxes'
+})
+export class CalculateTaxesPipe implements PipeTransform {
+
+  transform(item: MaintenanceItem): number {
+
+    let taxValue = 0;
+    for (const tax of item.lsTaxes) {
+      let taxTmp = item.referencePrice * (tax.percentValue / 100);
+      taxValue += taxTmp;
+    }
+    return taxValue;
+  }
+
+}
---
+++ src/app/Modules/items-and-routines/pipes/calculate-total-price.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { MaintenanceItem } from 'src/app/Models/MaintenanceItem';
+
+@Pipe({
+  name: 'calculateTotalPrice'
+})
+export class CalculateTotalPricePipe implements PipeTransform {
+
+  transform(item: MaintenanceItem): number {
+    let totalPrice = 0;
+    let taxValue = 0;
+    for (const tax of item.lsTaxes) {
+      let taxTmp = item.referencePrice * (tax.percentValue / 100);
+      taxValue += taxTmp;
+    }
+
+    totalPrice += item.referencePrice + taxValue;
+    return totalPrice;
+  }
+}
